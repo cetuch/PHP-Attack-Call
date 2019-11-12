cbc390d600b247817a53e2537103877b774ff9b5
Gamer_PernYren <45807795+PernYrenCraft@users.noreply.github.com>
2019-11-12 21:15:33 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+/* lobby.page.php */
+$CDN = "//localhost";
+$title = "สวัดดีผมทิน “เนอร์” ";
+$head = "~PernYrenCraft~";
+$sublife1 = "ดีคับ ผมชื่อ : พิณ [เพือนๆชอบเรียก ทิน]";
+$sublife2 = "งานอดิเรก : ขุดรหัส Hax,Spotify,Netflix,Minecraft ๆลๆ";
+$tel = "0652366373";
+$cadit = "Power | PernYrenCraft";
+$discord = "485833931283890188";
+$imageprofile = "https://avatars2.githubusercontent.com/u/45807795?v=4";
+
+/* นับจำนวนการเข้าชม */
+
+
+
+?>
+
+<?php
+
+/* login.page.php */
+$admin_userr = "demo";
+$admin_pass = "demo";
+
+?>
